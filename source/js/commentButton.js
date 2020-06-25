1ee9dba374771052903b71771ff70662df17f6ca
shurawi <denis.b.toropov@gmail.com>
2020-06-25 22:27:08 +0700

    вносит изменения в commetButton.js и index.html

--- source/js/commentButton.js
+++ source/js/commentButton.js
@@ -1,25 +1,25 @@
 
 /*В блоке 'comment' при нажатии на 'comment__button' переключает русскоязычные отзывы на англоязычные и на оборот.*/
 
-let commentContainerRu = document.querySelector('.comment__container--ru');
-let commentContainerEn = document.querySelector('.comment__container--en');
-let commentButtonRu = document.querySelector('.comment__button--ru');
-let commentButtonEn = document.querySelector('.comment__button--en');
+var commentContainerRu = document.querySelector('.comment__container--ru');
+var commentContainerEn = document.querySelector('.comment__container--en');
+var commentButtonRu = document.querySelector('.comment__button--ru');
+var commentButtonEn = document.querySelector('.comment__button--en');
 
-commentButtonRu.onclick = function() {
-  commentButtonRu.classList.toggle('comment__button--disable');
-  commentButtonRu.classList.toggle('comment__button--enable');
-  commentButtonEn.classList.toggle('comment__button--disable');
-  commentButtonEn.classList.toggle('comment__button--enable');
-  commentContainerRu.classList.toggle('comment__container--disable');
-  commentContainerEn.classList.toggle('comment__container--disable')
-}
+commentButtonRu.addEventListener ("click", function() {
+  commentButtonRu.classList.remove('comment__button--disable');
+  commentButtonRu.classList.add('comment__button--enable');
+  commentButtonEn.classList.remove('comment__button--enable');
+  commentButtonEn.classList.add('comment__button--disable');
+  commentContainerRu.classList.remove('comment__container--disable')
+  commentContainerEn.classList.add('comment__container--disable')
+})
 
-commentButtonEn.onclick = function() {
-  commentButtonEn.classList.toggle('comment__button--disable');
-  commentButtonEn.classList.toggle('comment__button--enable');
-  commentButtonRu.classList.toggle('comment__button--disable');
-  commentButtonRu.classList.toggle('comment__button--enable');
-  commentContainerEn.classList.toggle('comment__container--disable')
-  commentContainerRu.classList.toggle('comment__container--disable');
-}
+commentButtonEn.addEventListener ("click", function() {
+  commentButtonEn.classList.remove('comment__button--disable');
+  commentButtonEn.classList.add('comment__button--enable');
+  commentButtonRu.classList.add('comment__button--disable');
+  commentButtonRu.classList.remove('comment__button--enable');
+  commentContainerEn.classList.remove('comment__container--disable')
+  commentContainerRu.classList.add('comment__container--disable')
+})
